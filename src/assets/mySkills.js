1b6990e03fcac58e9ce29bbf2febaf2b129d5e3f
annu-1210 <annupoonia086@gmail.com>
2024-12-21 17:52:24 +0530

    content updated

--- src/assets/mySkills.js
+++ src/assets/mySkills.js
@@ -4,7 +4,9 @@ import img_3 from "./JAVASCRIPT-icon.svg";
 import img_4 from "./REACT-icon.svg";
 import img_5 from "./tailwindcss-icon.svg";
 import img_6 from "./nextjs.svg";
-import img_7 from "./git-logo.png";
+import img_7 from "./wordpress.png";
+import img_8 from "./shopify.png";
+import img_9 from "./git-logo.png";
 
 let skillSet = [
     {
@@ -39,8 +41,18 @@ let skillSet = [
     },
     {
         id: 7,
-        name: "GIT",
+        name: "WORDPRESS",
         image: img_7,
+    },
+    {
+        id: 8,
+        name: "SHOPIFY",
+        image: img_8,
+    },
+    {
+        id: 9,
+        name: "GIT",
+        image: img_9,
     },
 ]
 
